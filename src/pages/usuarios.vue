e0a796de43c4322f5fef8a304b1c2b7a856e0316
tchiteu <matheussantos.contato362@gmail.com>
2020-09-20 23:57:14 -0300

    Criando a tela de usuário

---
+++ src/pages/usuarios.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row>
+    <!-- TABELA -->
+    <v-col cols="8">
+      <v-data-table>
+
+      </v-data-table>
+    </v-col>
+    
+    <!-- CADASTRO -->
+    <v-col v-if="true" cols="4">
+    </v-col>
+
+    <!-- EDIÇÃO -->
+    <v-col v-else cols="4">
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      
+    },
+    methods: {
+
+    }
+  }
+</script>>
