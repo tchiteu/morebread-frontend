823bf2dc8317c7b67021a8e2199cf76c4d4fed83
tchiteu <matheussantos.contato362@gmail.com>
2020-09-25 12:21:53 -0300

    Finalizando lista de usuários

--- src/pages/usuarios.vue
+++ src/pages/usuarios.vue
@@ -1,29 +1,63 @@
 <template>
-  <v-row>
-    <!-- TABELA -->
-    <v-col cols="8">
-      <v-data-table>
+  <v-app>
+    <v-row class="mt-6 pa-4">
+      <!-- TABELA -->
+      <v-col cols="8">
+        <v-card class="pa-8 text-left custom-border" tile>
+          <h3 class="mb-8">Lista de usuários</h3>
 
-      </v-data-table>
-    </v-col>
-    
-    <!-- CADASTRO -->
-    <v-col v-if="true" cols="4">
-    </v-col>
+          <v-data-table 
+            class="elevation-2"
+            disable-sort
+            :headers="headers"
+            :items="usuarios"
+          />
+        </v-card>
+      </v-col>
+      
+      <!-- CADASTRO -->
+      <v-col v-if="true" cols="4">
+      </v-col>
 
-    <!-- EDIÇÃO -->
-    <v-col v-else cols="4">
-    </v-col>
-  </v-row>
+      <!-- EDIÇÃO -->
+      <v-col v-else cols="4">
+      </v-col>
+    </v-row>
+  </v-app>
 </template>
 
 <script>
   export default {
-    data: function() {
-      
+    mounted() {
+      this.buscaUsuarios();  
     },
-    methods: {
+    data: () => ({
+      usuarios: [],
 
+      headers: [
+        { text: "ID", value: "id" },
+        { text: "Nome", value: "nome" },
+        { text: "E-mail", value: "email" },
+        { text: "Cargo", value: "cargo" },
+        { text: "Ações", value: "acoes", width: "7%" }
+      ]
+    }),
+    methods: {
+      async buscaUsuarios() {
+        await this.$axios.get('/usuarios')
+          .then(retorno => {
+            this.usuarios = retorno.data;
+          })
+          .catch(err => {
+            if(err) this.usuarios = [];
+          })
+      }
     }
   }
-</script>>
+</script>
+
+<style scoped>
+  .custom-border {
+    border-top: solid 4px #674B4B;
+  }
+</style>
